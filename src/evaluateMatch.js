59dc3aa2fcec8148a0c8c6556f7d1eb99171ebe8
Gaël Haméon <gael.hameon@gmail.com>
2020-04-11 23:06:29 +0200

    working very simple version

--- src/evaluateMatch.js
+++ src/evaluateMatch.js
@@ -1,7 +1,10 @@
-
+const _ = require('lodash');
 
 function evaluateMatch(item, query) {
-
+  return Object.keys(query).every(queryKey => {
+    const queryValue = query[queryKey];
+    return item[queryKey] === queryValue;
+  });
 }
 
 module.exports = evaluateMatch;
--- test/feature/query-matcher.spec.js
+++ test/feature/query-matcher.spec.js
@@ -3,6 +3,25 @@ const { expect } = require('chai');
 const { evaluateMatch } = require('../../index');
 
 describe('evaluateMatch', () => {
+  context.only('given a very simple query object and a valid item', () => {
+    const query = { "key1": "value1" };
+    it('returns true if the item matches the query', () => {
+      const item = { "key1": "value1", "key2": "value2", key3: "value3" };
+      expect(evaluateMatch(item, query)).to.equal(true);
+    });
+    it('returns false if the item does not match the query', () => {
+      const item = { "key1": "otherValue", "key2": "value2", key3: "value3" };
+      expect(evaluateMatch(item, query)).to.equal(false);
+    });
+    it('returns false if the item is empty', () => {
+      const item = {};
+      expect(evaluateMatch(item, query)).to.equal(false);
+    });
+    it('returns false if the item has undefined keys not match the query', () => {
+      const item = {"key2": "toto"};
+      expect(evaluateMatch(item, query)).to.equal(false);
+    });
+  });
   context('given a valid query object a valid item', () => {
     const query = { "$or": [{ "firstTripPoint.trpptPlace": "SXR1" }, { "lastTripPoint.trpptPlace": "SXR2" }] };
     it('returns true if the item matches the query', () => {
@@ -19,10 +38,10 @@ describe('evaluateMatch', () => {
     it('returns false if the item does not match the query', () => {
       const item = {
         firstTripPoint: {
-          trpptPlace: "SXR1",
+          trpptPlace: "NAF1",
         },
         lastTripPoint: {
-          trpptPlace: "SXR2"
+          trpptPlace: "NAF2"
         }
       };
       expect(evaluateMatch(item, query)).to.equal(false);
