ae26d81dad957553e1b46af54951b9d4b1c44275
Gaël Haméon <gael.hameon@gmail.com>
2020-04-11 23:10:17 +0200

    works for simple cases

--- test/feature/query-matcher.spec.js
+++ test/feature/query-matcher.spec.js
@@ -3,10 +3,14 @@ const { expect } = require('chai');
 const { evaluateMatch } = require('../../index');
 
 describe('evaluateMatch', () => {
-  context.only('given a very simple query object and a valid item', () => {
+  context('given a very simple query object and a valid item', () => {
     const query = { "key1": "value1" };
     it('returns true if the item matches the query', () => {
       const item = { "key1": "value1", "key2": "value2", key3: "value3" };
+      expect(evaluateMatch(item, query)).to.equal(true);
+    });
+    it('returns true if the item matches the query', () => {
+      const item = { key1: "value1", "key2": "value2", key3: "value3" };
       expect(evaluateMatch(item, query)).to.equal(true);
     });
     it('returns false if the item does not match the query', () => {
@@ -22,7 +26,30 @@ describe('evaluateMatch', () => {
       expect(evaluateMatch(item, query)).to.equal(false);
     });
   });
-  context('given a valid query object a valid item', () => {
+  context('given a simple query object and a valid item', () => {
+    const query = { "key1": "value1", "key2": "value2" };
+    it('returns true if the item matches the query', () => {
+      const item = { "key1": "value1", "key2": "value2", key3: "value3" };
+      expect(evaluateMatch(item, query)).to.equal(true);
+    });
+    it('returns false if the item does not match the query', () => {
+      const item = { "key1": "otherValue", "key2": "value2", key3: "value3" };
+      expect(evaluateMatch(item, query)).to.equal(false);
+    });
+    it('returns false if the item does not match the query', () => {
+      const item = { "key1": "value1", "key2": "badValue", key3: "value3" };
+      expect(evaluateMatch(item, query)).to.equal(false);
+    });
+    it('returns false if the item is empty', () => {
+      const item = {};
+      expect(evaluateMatch(item, query)).to.equal(false);
+    });
+    it('returns false if the item has undefined keys not match the query', () => {
+      const item = {"key2": "toto"};
+      expect(evaluateMatch(item, query)).to.equal(false);
+    });
+  });
+  context.skip('given a valid query object a valid item', () => {
     const query = { "$or": [{ "firstTripPoint.trpptPlace": "SXR1" }, { "lastTripPoint.trpptPlace": "SXR2" }] };
     it('returns true if the item matches the query', () => {
       const item = {
